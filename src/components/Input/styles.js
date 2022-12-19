e1003cff8c79a7c6dc73e1842f04603fd99b05ca
Grégory Alvim <gregori.jagua@gmail.com>
2022-12-19 19:17:20 -0300

    Feat: styled the input component

---
+++ src/components/Input/styles.js
@@ -0,0 +1,21 @@
+import Styled from 'styled-components';
+
+export const Input = Styled.input`
+
+      width: 100%;
+      height: 5rem;
+
+      padding: 1.2rem;
+      margin-top: 0.8rem;
+
+      color: ${({ theme }) => theme.TEXT_COLORS.GRAY_PRIMARY };
+
+      background: none;
+
+      border-radius: 0.5rem;
+      border: 1px solid ${({ theme }) => theme.TEXT_COLORS.GRAY_SECONDARY };
+
+   &::placeholder {
+      color: ${({ theme }) => theme.TEXT_COLORS.GRAY_SECONDARY };
+   }
+`;
