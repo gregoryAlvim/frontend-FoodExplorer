b31a96457bbc6ae3bec9f031b30f920035e102b6
Grégory Alvim <gregori.jagua@gmail.com>
2022-12-19 19:16:47 -0300

    Feat: styled the button component

---
+++ src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import Styled from 'styled-components';
+
+export const Button = Styled.button`
+   width: 100%;
+   height: 5rem;
+
+   padding: 1.2rem;
+
+   font-family: "Poppins";
+   font-weight: 500;
+
+   color: ${({ theme }) => theme.TEXT_COLORS.PRIMARY };
+   background: ${({ theme }) => theme.BACKGROUND_COLORS.RED_PRIMARY};
+
+   border-radius: 0.5rem;
+   border: none;
+`;
