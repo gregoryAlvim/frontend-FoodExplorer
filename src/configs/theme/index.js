3d79ba1a9a641b92ba6ba6e1150d5e70d30ea50e
Grégory Alvim <gregori.jagua@gmail.com>
2022-12-24 11:41:46 -0300

    Feat: created file with the app themes

---
+++ src/configs/theme/index.js
@@ -0,0 +1,13 @@
+export const theme = {
+   BACKGROUND_COLORS: {
+      PRIMARY: "#000A0F",
+      SECONDARY: "#001119",
+      RED_PRIMARY: "#92000E",
+   },
+
+   TEXT_COLORS: {
+      PRIMARY: "#FFFFFF",
+      GRAY_PRIMARY: "#C4C4CC",
+      GRAY_SECONDARY: "#7C7C8A",
+   }
+}
