204120d362cac8b1b8e253205c4a61d11aa483c5
Grégory Alvim <gregori.jagua@gmail.com>
2022-12-24 11:38:56 -0300

    Feat: created the routes of app

---
+++ src/routes/index.jsx
@@ -0,0 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
+import { AuthRoutes } from "./auth.routes";
+
+export function Routes() {
+   return (
+      <BrowserRouter>
+         <AuthRoutes />
+      </BrowserRouter>
+   );
+}
