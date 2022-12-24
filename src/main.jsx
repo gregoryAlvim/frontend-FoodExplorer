f827e16079de7ef9fbc4b3d693d2e2abd43149f2
Grégory Alvim <gregori.jagua@gmail.com>
2022-12-24 11:40:16 -0300

    Feat: make the firsts configs to use a context

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,15 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from './configs/theme';
 import { Routes } from './routes/index';
 import { GlobalStyles } from './styles/global';
+import { AuthContext } from './hooks/auth';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
    <React.StrictMode>
       <ThemeProvider theme={theme}>
          <GlobalStyles />
-         <Routes />
+         <AuthContext.Provider value={{}}>
+            <Routes />
+         </AuthContext.Provider>
       </ThemeProvider>
    </React.StrictMode>
 )
