2eb44142e5c552de4a6ae664e2ef4bc3b64b3486
Grégory Alvim <gregori.jagua@gmail.com>
2022-12-28 17:29:39 -0300

    Feat: added the context auth to the app

--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,16 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from './configs/theme';
 import { Routes } from './routes/index';
 import { GlobalStyles } from './styles/global';
-import { AuthContext } from './hooks/auth';
+import { AuthProvider } from './hooks/auth';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
    <React.StrictMode>
       <ThemeProvider theme={theme}>
          <GlobalStyles />
-         <AuthContext.Provider value={{}}>
+         
+         <AuthProvider>
             <Routes />
-         </AuthContext.Provider>
+         </AuthProvider>
       </ThemeProvider>
    </React.StrictMode>
 )
