e45aa5f38d93edefdb7a076a02ca88e76d7de5bf
Grégory Alvim <gregori.jagua@gmail.com>
2022-12-28 17:43:32 -0300

    Feat: created a Header component app

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,50 @@
-export function Header() {
+import { FiSearch, FiLogOut } from "react-icons/fi";
+
+import { TitleComponent } from '../../components/Title';
+import { IconComponent } from '../Icon';
+import { TextButtonComponent } from '../../components/TextButton';
+import { ButtonComponent } from '../../components/Button';
+
+import { ReceiptIcon } from '../../assets/app-icons/Receipt/index';
+import imageIconFoodExplorer from '../../assets/PolygonIcon.png';
+
+import { Header, SearchContainer, Input } from "./styles";
+
+export function HeaderComponent() {
+
+   let purchaseRequestsCart = 0;
+
    return(
-      <>
-      
-      </>
+      <Header>
+         <TitleComponent className="foodExplorerTitle">
+            <IconComponent icon={imageIconFoodExplorer} className="foodExplorerIcon" />
+            food explorer
+         </TitleComponent>
+
+         <TextButtonComponent className="myFavoritesText">
+            Meus favoritos
+         </TextButtonComponent>
+
+         <SearchContainer>
+            <FiSearch
+               size={20}
+            />
+            <Input
+               type="text" 
+               placeholder="Busque pelas opções de pratos" 
+            />
+         </SearchContainer>
+         
+         <ButtonComponent 
+            className="purchaseRequestsButton"
+         >
+            <ReceiptIcon />
+            {`Meu pedido (${purchaseRequestsCart})`}
+         </ButtonComponent>
+
+         <TextButtonComponent className="logOutButton">
+            <FiLogOut size={20} />
+         </TextButtonComponent>
+      </Header>
    );
 }
