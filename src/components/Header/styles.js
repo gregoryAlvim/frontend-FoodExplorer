c1bd5c1a87dd3ee395231211a1c5bb56c6690770
Grégory Alvim <gregori.jagua@gmail.com>
2022-12-28 17:44:17 -0300

    Feat: styled the Header component and styled to laptop and mobile devices

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,6 +1,138 @@
 import Styled from 'styled-components';
+import { devices } from '../../configs/devices';
 
 export const Header = Styled.header`
    width: 100%;
-   background-color: red;
+   height: 10.4rem;
+
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   gap: 3.2rem;
+
+   padding: 2.4rem 12rem;
+
+   background: ${({ theme }) => theme.BACKGROUND_COLORS.SECONDARY};
+
+   & .foodExplorerIcon {
+      width: 3rem;
+   }
+
+   & .foodExplorerTitle {
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+
+      min-width: 20rem;
+
+      font-size: 2.5rem;
+      font-weight: 700;
+      line-height: 2.9rem;
+   }
+
+   & .myFavoritesText {
+      min-width: 12rem;
+      color: ${({ theme }) => theme.TEXT_COLORS.FAINT_GRAY};
+   }
+
+   & .purchaseRequestsButton {
+      min-width: 20rem;
+   }
+
+   & .logOutButton {
+      max-width: 5rem;
+   }
+
+   @media ${devices.laptop} {
+      width: 100%;
+      height: 100%;
+
+      display: grid;
+      grid-template:
+      'Title MyFavorite Logout'
+      'Search MyCart MyCart';
+
+      & .foodExplorerTitle {
+         grid-area: Title;
+      }
+
+      & .myFavoritesText {
+         grid-area: MyFavorite;
+      }
+
+      & .purchaseRequestsButton {
+         grid-area: MyCart;
+      }
+
+      & .logOutButton {
+         grid-area: Logout;
+      }
+   }
+
+   @media ${devices.mobile} {
+      min-width: 35rem;
+      height: 100%;
+
+      margin: 0 auto;
+
+      grid-template-rows: 10rem;
+      grid-auto-columns: 10rem;
+
+      grid-template-areas:
+      'Title Title Logout'
+      'MyCart MyCart MyCart'
+      'Search Search Search';
+
+      & .myFavoritesText {
+         display: none;
+      }
+
+      & .purchaseRequestsButton {
+         width: 100%;
+      }
+
+      & .logOutButton {
+         width: 10rem;
+      }
+   };
+`;
+
+export const SearchContainer = Styled.div`
+   display: flex;
+   align-items: center;
+   gap: 1rem;
+
+   min-width: 40rem;
+   height: 5rem;
+
+   padding: 1.2rem;
+
+   color: ${({ theme }) => theme.TEXT_COLORS.GRAY_PRIMARY };
+
+   background: none;
+
+   border-radius: 0.5rem;
+   border: 1px solid ${({ theme }) => theme.TEXT_COLORS.PRIMARY };
+
+   &::placeholder {
+      color: ${({ theme }) => theme.TEXT_COLORS.GRAY_SECONDARY };
+   }
+
+   @media ${devices.laptop} {
+      grid-area: Search;
+   }
+
+   @media ${devices.mobile} {
+      min-width: 1rem;
+   };
+`;
+
+export const Input = Styled.input`
+   width: 100%;
+
+   color: ${({ theme }) => theme.TEXT_COLORS.PRIMARY };
+
+   border: none;
+   outline: none;
+   background: none;
 `;
