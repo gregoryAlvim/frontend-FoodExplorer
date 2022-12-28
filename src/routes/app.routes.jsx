ed6ff403de17797b5a474ed6a4fcb7d14b4575ba
Grégory Alvim <gregori.jagua@gmail.com>
2022-12-28 17:34:38 -0300

    Feat: created the app routes

---
+++ src/routes/app.routes.jsx
@@ -0,0 +1,10 @@
+import { Route, Routes } from 'react-router-dom';
+import { Home } from '../pages/home';
+
+export function AppRoutes() {
+   return (
+      <Routes>
+         <Route path="/" element={<Home />} />
+      </Routes>
+   )
+}
