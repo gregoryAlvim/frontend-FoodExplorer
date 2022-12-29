fb450a84c16c8f7df31e9a1f93a59dd8e9dccdb5
Grégory Alvim <gregori.jagua@gmail.com>
2022-12-29 17:18:39 -0300

    Feat: created button component

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,11 +1,12 @@
+
 import { Button } from './styles';
 
-export function ButtonComponent({ ...rest }) {
+export function ButtonComponent({ children, ...rest }) {
    return(
       <Button 
          { ...rest }
       >
-
+         {children}
       </Button>
    );
 }
