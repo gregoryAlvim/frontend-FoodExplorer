87622aa6597768051e1219a9644e97305119c8e6
Grégory Alvim <gregori.jagua@gmail.com>
2022-12-29 17:24:20 -0300

    Feat: added new colors to the app theme

--- src/configs/theme/index.js
+++ src/configs/theme/index.js
@@ -1,12 +1,14 @@
 export const theme = {
    BACKGROUND_COLORS: {
       PRIMARY: "#000A0F",
-      SECONDARY: "#001119",
+      SECONDARY: "#00111A",
+      TERTIARY: "#001119",
       RED_PRIMARY: "#92000E",
    },
 
    TEXT_COLORS: {
       PRIMARY: "#FFFFFF",
+      FAINT_GRAY: "#E1E1E6",
       GRAY_PRIMARY: "#C4C4CC",
       GRAY_SECONDARY: "#7C7C8A",
    }
