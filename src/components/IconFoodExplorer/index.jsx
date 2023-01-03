1d389e7a799cc8eab9e04332f86af838eeb3a6a6
Grégory Alvim <gregori.jagua@gmail.com>
2023-01-02 22:08:23 -0300

    Refactor: deleted file

--- src/components/IconFoodExplorer/index.jsx
+++
@@ -1,8 +0,0 @@
-import { IconImage } from './styles';
-import imageIconFoodExplorer from '../../assets/PolygonIcon.png';
-
-export function IconFoodExplorerComponent({ ...rest }) {
-   return(
-      <IconImage src={imageIconFoodExplorer} {...rest} />
-   );
-}
