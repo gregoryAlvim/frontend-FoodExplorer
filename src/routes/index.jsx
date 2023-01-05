ea36fdb6ee856843f012827d063dd33bc30e2378
Grégory Alvim <gregori.jagua@gmail.com>
2023-01-04 22:05:53 -0300

    Refactor: some change

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,7 +6,7 @@ import { useAuth } from '../hooks/auth';
 
 export function Routes() {
    const { user } = useAuth();
-
+   
    return (
       <BrowserRouter>
          { user ? <AppRoutes /> : <AuthRoutes /> }
