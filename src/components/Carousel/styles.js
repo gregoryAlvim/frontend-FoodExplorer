e4a9da36e367cb7e4edb450fc374850c19b7302f
Grégory Alvim <gregori.jagua@gmail.com>
2023-01-05 22:14:32 -0300

    Refactor: removed some  unused styles

--- src/components/Carousel/styles.js
+++ src/components/Carousel/styles.js
@@ -12,7 +12,7 @@ export const Container = Styled.div`
 
       margin: 5rem 0 3rem 0;
 
-      color: ${({theme}) => theme.TEXT_COLORS.FAINT_GRAY};
+      color: ${({ theme }) => theme.TEXT_COLORS.FAINT_GRAY};
  
    }
 
@@ -79,48 +79,24 @@ export const Container = Styled.div`
    }
 
    @media ${devices.laptop} {
-      & .titleCarousel {
-      font-family: 'Poppins';
-      font-weight: 500;
-      font-size: 3.2rem;
 
-      margin: 5rem 0 3rem 0;
-
-      color: ${({theme}) => theme.TEXT_COLORS.FAINT_GRAY};
-
-      text-align: center;
- 
-      }
-
-      & .rightOpacityEffect {
-      background: 
-         linear-gradient(
-            to right,
-            #000A0F 40%,
-            rgba(0, 10, 15, 0.272541) 90%,
-            rgba(0, 10, 15, 0)
-         );
-         position: absolute;
-         height: 100%;
-         width: 10rem;
-         z-index: 1;
-         content: "";
-   }
-
-   & .leftOpacityEffect {
-      background: 
-         linear-gradient(
-            to left,
-            #000A0F 40%,
-            rgba(0, 10, 15, 0.272541) 90%,
-            rgba(0, 10, 15, 0)
-         );
-         position: absolute;
-         height: 100%;
-         width: 10rem;
-         z-index: 1;
-         content: "";
-         right: 0;
-   }
    };
+
+   @media ${devices.mobile} {
+         flex-direction: column;
+         gap: 1rem;
+
+         height: 100%;
+
+         
+         text-align: center;
+
+         & .rightOpacityEffect {
+            width: 5rem;
+         }
+
+         & .leftOpacityEffect {
+               width: 5rem;
+         }
+      }
 `;
