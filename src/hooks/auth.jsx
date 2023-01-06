1bdaf0ab8613a33c7f039f8266605126b6119f5f
Grégory Alvim <gregori.jagua@gmail.com>
2023-01-05 22:23:18 -0300

    Feat: create the function to user sign out of app

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -29,6 +29,13 @@ function AuthProvider({ children }) {
       }
    };
 
+   function signOut() {
+         localStorage.removeItem("@foodexplorer:user");
+         localStorage.removeItem("@foodexplorer:token");
+
+         setData({});
+   };
+
    useEffect(() => {
       const user = localStorage.getItem('@foodexplorer:user');
       const token = localStorage.getItem('@foodexplorer:token');
@@ -45,7 +52,7 @@ function AuthProvider({ children }) {
    }, []);
 
    return (
-      <AuthContext.Provider value={{ signIn, user: data.user }}>
+      <AuthContext.Provider value={{ signIn, signOut, user: data.user }}>
          {children}
       </AuthContext.Provider>
    )
