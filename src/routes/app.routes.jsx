1c337df5ab0a3c56d6200402d7a69ee5aaac2794
Grégory Alvim <gregori.jagua@gmail.com>
2023-01-05 22:24:57 -0300

    Feat: some changes

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
-import { Home } from '../pages/home';
+import { Home } from '../pages/Home';
+import { NewDish } from '../pages/NewDish';
 
 export function AppRoutes() {
    return (
       <Routes>
          <Route path="/" element={<Home />} />
+         <Route path="/create-dish" element={<NewDish />} />
       </Routes>
    )
 }
