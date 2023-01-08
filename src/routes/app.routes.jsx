2659118baad25037b8e95d47493f8dafd8bc1b7a
Grégory Alvim <gregori.jagua@gmail.com>
2023-01-07 22:25:13 -0300

    Feat: added new route, edit dish page

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { NewDish } from '../pages/NewDish';
+import { EditDish } from '../pages/EditDish';
 
 export function AppRoutes() {
    return (
       <Routes>
          <Route path="/" element={<Home />} />
          <Route path="/create-dish" element={<NewDish />} />
+         <Route path="/update-dish/:id" element={<EditDish />} />
       </Routes>
    )
 }
