ae8ce4d6b13181b4c6057162fd0a8eb189cfd6eb
Grégory Alvim <gregori.jagua@gmail.com>
2023-01-07 22:30:50 -0300

    Feat: added new methods to Admin, edit dish and delete dish

--- src/components/ItemCarousel/index.jsx
+++ src/components/ItemCarousel/index.jsx
@@ -1,6 +1,8 @@
+import { FiHeart, FiEdit, FiMinus, FiPlus, FiX } from 'react-icons/fi';
+import { useNavigate } from 'react-router-dom';
 
-
-import { FiHeart, FiMinus, FiPlus } from 'react-icons/fi';
+import { api } from '../../services/api';
+import { useAuth } from '../../hooks/auth';
 
 import { IconComponent } from '../Icon';
 import { ParagraphComponent } from '../Paragraph';
@@ -9,20 +11,49 @@ import { TextButtonComponent } from '../TextButton';
 
 import noImage from '../../assets/noImage.png';
 
-import { Container, Span, Section, IncludeDishContainer } from './styles';
+import { Container, Span, Section, IncludeDishContainer, ButtonsContainer } from './styles';
 
-export function ItemCarouselComponent({ dishImage, name, description, price }) {
+export function ItemCarouselComponent({ dishId ,dishImage, name, description, price }) {
+
+   const navigate = useNavigate();
+   const { user } = useAuth();
+   
+   const isAdmin = user.role === "Admin";
+   
+   const dishImageURL = dishImage ? `${api.defaults.baseURL}/files/${dishImage}` : noImage;
 
    let amountDishIncluded = 1;
 
+   function redirectToUpdateDish() {
+      navigate(`/update-dish/${dishId}`);
+   }
+
    return (
       <Container>
-            <TextButtonComponent className="heartButton">
+         <ButtonsContainer>
+            <TextButtonComponent className="buttons">
                <FiHeart size={20} />
             </TextButtonComponent>
+
+            {
+               isAdmin ? <TextButtonComponent 
+                  className="buttons"
+                  onClick={redirectToUpdateDish}
+               >
+                  <FiEdit size={20} />
+               </TextButtonComponent> : null
+            }
+            
+            {
+               isAdmin ? <TextButtonComponent className="buttons">
+                  <FiX color={"#92000E"} size={20} />
+               </TextButtonComponent> : null
+            }
+         </ButtonsContainer>
+
          <Section>
             
-            <IconComponent className="dishImage" icon={ dishImage ?? noImage } />
+            <IconComponent className="dishImage" icon={dishImageURL} />
 
             <Span className="dishInfoName">{ `${name} >` }</Span>
 
