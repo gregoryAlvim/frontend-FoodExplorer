9c34cd454f629b61e700727f900545d6d031b5c3
Grégory Alvim <gregori.jagua@gmail.com>
2023-01-08 17:10:39 -0300

    Refactor: removed useEffect, passed to carousel component

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,5 @@
 import { useState, useEffect } from 'react';
 
-import { api } from '../../services/api';
-
 import { HeaderComponent } from '../../components/Header';
 import { SubTitleComponent } from '../../components/SubTitle';
 import { ParagraphComponent } from '../../components/Paragraph';
@@ -15,43 +13,12 @@ import { Main, Image, CoverPageContainer } from './styles';
 export function Home() {
 
    const [search, setSearch] = useState("");
-   const [dishes, setDishes] = useState([""]);
-   const [desserts, setDesserts] = useState([""]);
-   const [drinks, setDrinks] = useState([""]);
-
-   useEffect(() => {
-
-      async function fetchDishes() {
-         try {
-
-            const response = await api.get(`dishes/index-dishes?dishName${search}`);
-            // setDishes(response.data);
-            const foodData = response.data.filter((dish) => dish.category === "Comida");
-            setDishes(foodData);
-
-            const dessertsData = response.data.filter((dish) => dish.category === "Sobremesa");
-            setDesserts(dessertsData);
-
-            const drinksData = response.data.filter((dish) => dish.category === "Bebida");
-            setDrinks(drinksData);
-
-         } catch (error) {
-
-            if (error.response) {
-               alert(error.response.data.message);
-            } else {
-               alert(" Algo deu errado ao buscar os pratos! ");
-            }
-         }
-      }
-
-      fetchDishes();
-
-   }, [search]);
 
    return (
       <>
-         <HeaderComponent />
+         <HeaderComponent 
+            onChange={(event) => setSearch(event.target.value)}
+         />
 
          <Main>
             <CoverPageContainer>
@@ -66,15 +33,14 @@ export function Home() {
                </ParagraphComponent>
             </CoverPageContainer>
 
-            <CarouselComponent description="Pratos principais" selectCategory="Comida" dishes={dishes} />
+            <CarouselComponent description="Pratos principais" selectCategory="Comida" searchHeader={search} />
             
-            <CarouselComponent description="Sobremesas" selectCategory="Sobremesa" dishes={desserts} />
+            <CarouselComponent description="Sobremesas" selectCategory="Sobremesa" searchHeader={search} />
 
-            <CarouselComponent description="Bebidas" selectCategory="Bebida" dishes={drinks} />
+            <CarouselComponent description="Bebidas" selectCategory="Bebida" searchHeader={search} />
          </Main>
 
          <FooterComponent />
-         {console.log(".")}
       </>
    );
 }
