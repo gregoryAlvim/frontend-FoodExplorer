d39f7eb676cc76cb4df529046ecaec6c42174183
Grégory Alvim <gregori.jagua@gmail.com>
2023-01-12 15:49:32 -0300

    Feat: added responsive styles

--- src/pages/DetailDish/styles.js
+++ src/pages/DetailDish/styles.js
@@ -2,15 +2,20 @@ import Styled from 'styled-components';
 import { devices } from '../../configs/devices';
 
 export const Main = Styled.main`
-   padding: 3.2rem 30rem 0;
+   padding: 13.5rem 25rem 0;
+   height: 100%;
+   position: relative;
 
    display: flex;
    flex-direction: column;
 
    & .backButton {
+      
       width: 11rem;
       text-align: start;
       font-size: 2.4rem;
+
+      margin-bottom: 3rem;
 
       font-family: 'Poppins';
       font-weight: 500;
@@ -24,29 +29,16 @@ export const Main = Styled.main`
    }
 
    @media ${devices.laptop} {
-      padding: 3.2rem 10rem 0;
+      padding: 20rem 1rem 0;
    }
 
    @media ${devices.mobile} {
-      padding: 3.2rem 2rem 0;
-   }
-`;
-
-export const Container = Styled.div`
-   display: flex;
-`;
-
-export const DishInfoContainer = Styled.div`
-   display: flex;
-   flex-direction: column;
-
-   color: ${({ theme }) => theme.TEXT_COLORS.FAINT_GRAY};
-
-   & .dishDescription {
-      font-size: 2.4rem;
-      font-family: 'Poppins';
-      font-weight: 400;
+      padding: 30rem 2rem 0;
       
+      & .dishImage {
+         width: 70%;
+         height: 70%;
+      }
    }
 `;
 
@@ -60,7 +52,92 @@ export const Span = Styled.span`
 
    &.dishPrice {
       font-size: 3.2rem;
+      min-width: 13rem;
       color: ${({theme}) => theme.TEXT_COLORS.FAINT_BLUE};
+      margin-right: 2rem;
    }
 
+   @media ${devices.laptop} {
+      margin-right: 0rem;
+   }
+
+   @media ${devices.mobile} {
+      &.dishInfoName {
+         font-size: 3.7rem;
+      }
+   }
+
+`;
+
+export const Container = Styled.div`
+   display: flex;
+   gap: 4rem;
+
+   @media ${devices.mobile} {
+      flex-direction: column;
+      align-items: center;
+   }
+`;
+
+export const DishInfoContainer = Styled.div`
+   display: flex;
+   flex-direction: column;
+   gap: 1rem;
+
+   color: ${({ theme }) => theme.TEXT_COLORS.FAINT_GRAY};
+
+   & .dishDescription {
+      font-size: 2rem;
+      font-family: 'Poppins';
+      font-weight: 400;
+   }
+
+   @media ${devices.mobile} {
+      align-items: center;
+
+      & .dishDescription {
+         text-align: center;
+      }
+   }
+`;
+
+export const PriceAndIncludeContainer = Styled.div`
+   display: flex;
+   align-items: center;
+   gap: 4rem;
+
+   @media ${devices.laptop} {
+      gap: .3rem;
+   }
+`;
+
+export const IncludeDishContainer = Styled.div`
+   display: flex;
+   align-items: center;
+   gap: 1.2rem;
+
+   color: ${({theme}) => theme.TEXT_COLORS.GRAY_PRIMARY};
+
+   & .addButton {
+      width: 10rem;
+   }
+`;
+
+export const IncludeDishButtonsContainer = Styled.div`
+   display: flex;
+   align-items: center;
+   gap: 1.2rem;
+
+   color: ${({theme}) => theme.TEXT_COLORS.GRAY_PRIMARY};
+
+   & .minusAndPlusButton {
+      display: flex;
+      align-items: center;
+      width: 3rem;
+   }
+
+   & .dishAmount {
+      font-weight: 700;
+      font-size: 2rem;
+   }
 `;
