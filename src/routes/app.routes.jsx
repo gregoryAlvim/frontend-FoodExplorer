1f218e7736d7fd5af7cd2f63e84abd88c4acb62e
Grégory Alvim <gregori.jagua@gmail.com>
2023-01-12 22:17:12 -0300

    Feat: some changes

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from 'react-router-dom';
-
 import { Home } from '../pages/Home';
 import { NewDish } from '../pages/NewDish';
 import { EditDish } from '../pages/EditDish';
